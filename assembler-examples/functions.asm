d36ebd9c5f05401f28d67e08a1de2e58e059def3
Michał <121185944+Demichu5@users.noreply.github.com>
2026-01-07 22:04:26 +0100

    Add files via upload

---
+++ assembler-examples/functions.asm
@@ -0,0 +1,17 @@
+; functions.asm
+; Cel: Wywolac funkcje 'DODAJ_DWA' 3 razy.
+; Wynik w R1 powinien wynosic: 0 -> 2 -> 4 -> 6.
+
+.code
+fn MAIN
+    set r1
+    mov 0
+    call DODAJ_DWA
+    call DODAJ_DWA
+    call DODAJ_DWA
+    halt
+
+fn DODAJ_DWA
+    inc r1
+    inc r1
+    ret
